5be41da5a29ff0c9414b5a589b15bcc8c7a7d0ff
Andres Jejen <andresjt93@gmail.com>
2022-03-24 16:25:09 -0500

    ES6 Classes

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-underscore-dangle */
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // setters
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
